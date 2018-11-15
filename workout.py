a2321fed40d9dbb8cffa0ba002177aa64ac30d54
Kamil Banks <kamilbanks@brandeis.edu>
2018-11-15 11:54:01 -0500

    Defined function to ask questions determining fitness level.

---
+++ workout.py
@@ -0,0 +1,23 @@
+def input():
+    print ("What is your name?")
+    print ("What is your age?")
+    print ("What is your weight?")
+t=str(input("In the past week, how mnay days have you worked out?"))
+s=str(input("How long you workout for?"))
+n=str(input("How many push ups can you do?"))
+w=str(input("How many sit ups/crunches can you do?"))
+
+profile = []
+information = []
+
+def retriveInput(q):
+    i = input(q)
+    return i
+
+a1 = retriveInput("What is your name?")
+a2 = retriveInput("What is your age?")
+a3 = retriveInput("What is your weight?")
+
+profile.append(a1)
+profile.append(a2)
+profile.append(a3)
