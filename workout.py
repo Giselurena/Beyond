8bc6f0d4f2f2472616ac90281c85ad287ca8d0ef
kamilb123 <44580086+kamilb123@users.noreply.github.com>
2018-11-19 11:30:18 -0500

    Update workout.py

--- workout.py
+++ workout.py
@@ -1,8 +1,11 @@
-def input():
-    print ("What is your name?")
-    print ("What is your age?")
-    print ("What is your weight?")
-t=str(input("In the past week, how mnay days have you worked out?"))
+
+name= input("What is your name?")
+print ("What is your age?")
+print ("What is your weight in pounfd?")
+
+print("Hello "+name+" how are you?")
+
+t=str(input("how many days have you worked out?"))
 s=str(input("How long you workout for?"))
 n=str(input("How many push ups can you do?"))
 w=str(input("How many sit ups/crunches can you do?"))
