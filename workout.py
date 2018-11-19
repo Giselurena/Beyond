3e75fb7f0e329e33d5bb47cf14f63769ad1fb29f
kamilb123 <44580086+kamilb123@users.noreply.github.com>
2018-11-19 16:29:51 -0500

    Took off ending, so we could continue asking questions

--- workout.py
+++ workout.py
@@ -1,30 +1,14 @@
 
-name= input("What is your name?")
-age=input("What is your age?")
-weight=input("What is your weight in pounds?")
-height=input("How tall are you in inches?")
+name= input("What is your name? ")
+age=input("What is your age? ")
+weight=input("What is your weight in pounds? ")
+height=input("How tall are you in inches? ")
 
-print("Hello "+name+" Welcome to Beyond Fitness!")
+print("Hello "+name+" Welcome to Beyond Fitness! ")
 
-t=str(input("how many days have you worked out in the past week?"))
-s=str(input("How long do you usually workout for in minutes ?"))
+t=str(input("how many days have you worked out in the past week? "))
+s=str(input("How long do you usually workout for in minutes ? "))
 
-goals=input("What is your main priority? Your legs, arms or stomach?")
+goals=input("What is your main priority? Your legs, arms or stomach ? ")
 
-repeat=input("So"+name+"you weigh"+weight+"pounds, are"+age" years old, and want to work out your"+goals+" Correct?")
-
-
-profile = []
-information = []
-
-def retriveInput(q):
-    i = input(q)
-    return i
-
-a1 = retriveInput("What is your name?")
-a2 = retriveInput("What is your age?")
-a3 = retriveInput("What is your weight?")
-
-profile.append(a1)
-profile.append(a2)
-profile.append(a3)
+print("So "+name+" you weigh "+weight+" pounds, are "+age+ " years old, and want to work out your "+goals+" Correct?")
