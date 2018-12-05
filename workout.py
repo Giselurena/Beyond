a77d63efd7e612c0e973d6e8e4e22bfedf11a210
Giselurena <giselurena@brandeis.edu>
2018-12-05 11:43:18 -0500

    12.5.18

--- workout.py
+++ workout.py
@@ -6,9 +6,9 @@ height=input("How tall are you in inches? ")
 
 print("Hello "+name+" Welcome to Beyond Fitness! ")
 
-t=str(input("how many days have you worked out in the past week? "))
-s=str(input("How long do you usually workout for in minutes ? "))
+t=str(input("How many days have you worked out in the past week? "))
+s=str(input("How long do you usually workout for in minutes?"))
 
-goals=input("What is your main priority? Your legs, arms or stomach ? ")
+goals=input("What is your main priority? Your legs, arms or stomach?")
 
 print("So "+name+" you weigh "+weight+" pounds, are "+age+ " years old, and want to work out your "+goals+" Correct?")
